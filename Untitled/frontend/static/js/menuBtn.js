05f51b22324c1db349638aab55927d6782ab1c48
Lesha <lexaxiip@gmail.com>
2024-02-20 20:59:48 +0200

    create navbar

--- Untitled/frontend/static/js/menuBtn.js
+++ Untitled/frontend/static/js/menuBtn.js
@@ -1,5 +1,6 @@
 const tabItem = document.querySelectorAll('.tabs__btn-item')
 const tabContent = document.querySelectorAll('.tabs__content-item')
+const svgItem = document.querySelectorAll('.tabs__btn-item-svg')
 
 tabItem.forEach(function (element) {
 	element.addEventListener('click', open)
@@ -22,4 +23,18 @@ function open(evt) {
 	document
 		.querySelector(`#${button}`)
 		.classList.add('tabs__content-item--active')
+}
+
+svgItem.forEach(function (element) {
+	element.addEventListener('click', clickSvg)
+})
+
+function clickSvg(evt) {
+	const svgTarget = evt.currentTarget
+
+	svgItem.forEach(function (item) {
+		item.classList.remove('tabs__btn-item-svg--active')
+	})
+
+	svgTarget.classList.add('tabs__btn-item-svg--active')
 }
--- Untitled/main/views.py
+++ Untitled/main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def main_view(request):
-    return render(request, 'test.html')
+    return render(request, 'index.html')
