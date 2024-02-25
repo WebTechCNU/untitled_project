2ca98137eb47dd0befe02ac6778469a911b243f6
Lesha <lexaxiip@gmail.com>
2024-02-25 10:38:53 +0200

    fix registers url

--- Untitled/main/urls.py
+++ Untitled/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', main_view, name="Main"),
+    path('', main_view, name="Home"),
+    path('registers/', register, name="Registration")
 ]
--- Untitled/main/views.py
+++ Untitled/main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def main_view(request):
      return render(request, 'index.html')
+
+def register(request):
+     return render(request, 'register.html')
